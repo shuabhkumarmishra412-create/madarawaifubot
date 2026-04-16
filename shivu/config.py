c4dcf2dad38607e92553869d49992f0a4f116645
shuabhkumarmishra412-create <shuabhkumarmishra412@gmail.com>
2026-04-16 14:00:45 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = "8441236350"
+    sudo_users = "7553434931", "8725331299"
+    GROUP_ID = -1003992204811
+    TOKEN = "8264339422:AAEDnkBcUgK_ul-Ivjq32i_lkrkPZE_pPlU"
+    mongo_url = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    PHOTO_URL = ["https://files.catbox.moe/aw6zui.jpg"]
+    SUPPORT_CHAT = "https://t.me/+dv_rcq5uIXhmMWM1"
+    UPDATE_CHAT = "https://t.me/+Imyf3M9TO5k1ODRl"
+    BOT_USERNAME = "@WAIFU_SLAVE_OP_bot"
+    CHARA_CHANNEL_ID = "abrakatabragiligilichu"
+    api_id = 35411328
+    api_hash = "4c8d3c8f5d3483296f5fb530ea2cfcc6"
 
     
 class Production(Config):
